2c377e6bf32eda7237388265f115371603a62e47
Jake Mays <quaunaut@gmail.com>
2014-04-23 01:41:56 -0500

    Initial commit. We've begun.

---
+++ Gemfile
@@ -0,0 +1,43 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'spring'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'ember-rails'
+gem 'ember-source'
+gem 'emblem-rails'
+gem 'active_model_serializers'
+gem 'rails-console-tweaks'
+gem 'bootstrap-sass'
+gem 'pry-rails'
+gem 'dotenv-rails'
+gem 'puma'
+gem 'haml'
+gem 'valid_email'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'rubocop'
+  gem 'jshintrb'
+  gem 'brakeman'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'simplecov', require: false
+  gem 'webrat'
+  gem 'should_not'
+  gem 'webmock'
+  gem 'factory_girl_rspec'
+  gem 'bundler-audit'
+end
+
+group :ct do
+  gem 'guard-rspec'
+  gem 'guard-rubocop'
+  gem 'guard-jshintrb'
+end
---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../boot', __FILE__)
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Maaku
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config.autoload_paths << config.root.join('lib', 'autoloaded')
+  end
+end
